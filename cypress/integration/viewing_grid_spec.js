0d97e1c4c11b420ea42e9a22f818477be8ec52b5
josh nickson <jnickson1506@gmail.com>
2018-10-10 18:13:28 +0100

    test for visit page and see title

---
+++ cypress/integration/viewing_grid_spec.js
@@ -0,0 +1,7 @@
+describe('Viewing the grid', () => {
+  it('visits the page and sees grid', () => {
+    cy.visit('http://localhost:3000');
+    cy.get('h1')
+      .should('contain', 'Game of Life')
+  });
+});
